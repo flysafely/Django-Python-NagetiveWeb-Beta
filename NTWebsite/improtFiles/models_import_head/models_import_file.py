186c44fe14b6fe602e93ddd36791fdf98f74541c
flysafely <616604060@qq.com>
2019-05-17 20:05:41 +0800

    权限功能完善

--- NTWebsite/improtFiles/models_import_head/models_import_file.py
+++ NTWebsite/improtFiles/models_import_head/models_import_file.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from django.db.models import Q
 from django.db.models import F
+from django.db.models.query import QuerySet
+from django.db.models import Model
 from ...models import *
